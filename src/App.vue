2a0bdfeb6cd56daf57bbeea15ec933b3cd251101
李思柰 <lxx328825321@sina.com>
2021-09-26 21:55:00 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="来呀，快活呀"/>
   </div>
 </template>
 
